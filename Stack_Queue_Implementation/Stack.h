4d6e7d4627a6d2054e7e10dd0e2b3f5365941cc4
ProffD <tbmatome@gmail.com>
2018-06-27 18:19:28 +0200

    Updated the Stack implementation to use templates

--- Stack_Queue_Implementation/Stack.h
+++ Stack_Queue_Implementation/Stack.h
@@ -3,18 +3,19 @@
 #include<memory>
 //#define MAX_SIZE 10
 
-
+template<typename T>
 class Stack
 {
 public:	
 	Stack(int size);
 	bool isEmpty()const;
-	int peek()const;
-	void push(int var);
+	T peek()const;
+	void push(T var);
 	void pop();
+	void displayStack();
 	~Stack();
 private:
-	std::unique_ptr<int[]> arr;
+	std::unique_ptr<T[]> arr;
 	//int arr[MAX_SIZE];
 	int max_size;
 	int top;
@@ -22,3 +23,67 @@ private:
 	
 };
 
+
+template<typename T>
+Stack<T>::Stack(int size)
+{
+	arr = std::make_unique<T[]>(size);
+	if (arr == nullptr)
+	{
+		std::cerr << "Could not allocate memory indicated!" << '\n';
+		exit(1);
+	}
+	max_size = size;
+	top = -1;
+}
+template<typename T>
+bool Stack<T>::isEmpty()const
+{
+	if (top < 0)
+		return true;
+	else
+		return false;
+}
+template<typename T>
+T Stack<T>::peek()const
+{
+	if (isEmpty()) {
+		std::cerr << "Stack is empty!" << '\n';
+		return -1;
+	}
+	else
+		return arr[top];
+}
+template<typename T>
+void Stack<T>::push(T var)
+{
+
+	if (top == max_size - 1)
+		std::cerr << "Stack is full!!\nMaximum size is: " << max_size << '\n';
+	else {
+		top++;
+		arr[top] = var;
+	}
+
+
+}
+template<typename T>
+void Stack<T>::pop()
+{
+	if (isEmpty())
+		std::cerr << "Stack is empty!" << '\n';
+	else
+		top--;
+}
+template<typename T>
+void Stack<T>::displayStack()
+{
+	while (!isEmpty()) {
+		std::cout << peek() << " ";
+		pop();
+	}
+}
+template<typename T>
+Stack<T>::~Stack()
+{
+}
